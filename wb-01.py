a7849f48eca8a9cc78e8960dd71896f169e21e2a
Dmitrijs <dmitrijs@modulai.io>
2022-10-27 20:54:24 +0200

    WIP

--- wb-01.py
+++ wb-01.py
@@ -3,7 +3,7 @@ import wandb
 
 config = {"sample_size": 500, "n_samples": 200}
 
-wandb.init(project="LB", entity="dmitrijs-modulai", config=config)
+wandb.init(project="LB1", entity="dmitrijs-modulai", config=config)
 
 running_mean = 0
 samples = []
@@ -13,9 +13,5 @@ for iter in range(1, config["n_samples"] + 1):
     sample = np.random.randn(config["sample_size"])
     sample_mean = np.mean(sample)
     running_mean += 1 / iter * (sample_mean - running_mean)
-
-    samples.append(sample)
-    means.append(sample_mean)
-    print(running_mean, np.mean(samples), np.mean(means))
 
     wandb.log({"running_mean": running_mean, "sample": sample})
